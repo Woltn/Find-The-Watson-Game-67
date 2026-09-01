6b75135f0cd11b091d68b581b776bc9cc5829d80
Woltn <watson.faulkner@student.mcdonald.nsw.edu.au>
2026-09-01 14:35:08 +1000

    Update script.js

--- script.js
+++ script.js
@@ -318,13 +318,13 @@ console.log("");
 
 const backgrounds = [
 
-    "assets/background1.jpg",
-    "assets/background2.jpg",
-    "assets/background3.jpg",
-    "assets/background4.jpg",
-    "assets/background5.jpg",
-    "assets/background6.jpg",
-    "assets/background7.jpeg"
+    "background1.jpg",
+    "background2.jpg",
+    "background3.jpg",
+    "background4.jpg",
+    "background5.jpg",
+    "background6.jpg",
+    "background7.jpeg"
 
 ];
 
